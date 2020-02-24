0df28cb0cd62539cf80717a831bfe41e67c26163
Kirpalsinh9 <kirpalsinhji976@yahoo.com>
2020-02-24 16:05:33 -0500

    Initial Commit

---
+++ Pages/index.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const Index = () => (
+    <Layout>
+        <div>
+            <h1>Welcom to NextJS</h1>
+
+        </div>
+    </Layout>
+);
+
+export default Index;
